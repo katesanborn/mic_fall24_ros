e50342b9e94f91e25a97a0afc3cafcb59a95d0e9
Kate Sanborn <kate.l.sanborn@vanderbilt.edu>
2024-11-24 23:10:33 -0600

    Added connections with group pubs and subs

--- src/plugins/MakeConnections/MakeConnections/__init__.py
+++ src/plugins/MakeConnections/MakeConnections/__init__.py
@@ -26,6 +26,9 @@ class MakeConnections(PluginBase):
         subscribers = []
         topics = []
         remaps = []
+        groups = []
+        group_pubs = []
+        group_subs = []
         
         def at_node(node):
             meta_node = core.get_base_type(node)
@@ -42,6 +45,12 @@ class MakeConnections(PluginBase):
                 topics.append(node)
             if meta_type == "Remap":
                 remaps.append(node)
+            if meta_type == "Group":
+                groups.append(node)
+            if meta_type == "GroupPublisher":
+                group_pubs.append(node)
+            if meta_type == "GroupSubscriber":
+                group_subs.append(node)
             
         def get_connectable_ports(node):
             pubs = []
@@ -53,9 +62,9 @@ class MakeConnections(PluginBase):
                 for g in grand_children:
                     meta_node = core.get_base_type(g)
                     meta_type = core.get_attribute(meta_node, 'name')
-                    if meta_type == "Publisher":
+                    if meta_type == "Publisher" or meta_type == "GroupPublisher":
                         pubs.append(g)
-                    if meta_type == "Subscriber":
+                    if meta_type == "Subscriber" or meta_type == "GroupSubscriber":
                         subs.append(g)
                     
             return (pubs, subs)
@@ -70,12 +79,45 @@ class MakeConnections(PluginBase):
             core.set_pointer(new_topic, 'dst', sub)
             core.set_attribute(new_topic, 'name', name)
             
-        # Get list of publishers, subscribers, and topics    
+        # Get list of publishers, subscribers, topics, group pubs, group subs    
         self.util.traverse(active_node, at_node)
         
-        # Delete all existing topics connections
+        # Delete all existing topics, group pubs, group subs
         for t in topics:
             core.delete_node(t)
+        
+        for p in group_pubs:
+            core.delete_node(p)
+        
+        for s in group_subs:
+            core.delete_node(s)
+        
+        # Add new group nodes
+        for g in groups:
+            pubs_in_g = []
+            subs_in_g = []
+            def create_group_pub_sub(node):
+                meta_node = core.get_base_type(node)
+                name = core.get_attribute(node, 'name')
+                path = core.get_path(node)
+                meta_type = 'undefined'
+                if meta_node:
+                    meta_type = core.get_attribute(meta_node, 'name')
+                
+                parent = core.get_parent(node)
+                parent_name = core.get_attribute(parent, "name")
+                
+                if meta_type == "Publisher":
+                    new_group_pub = core.create_child(g, self.util.META(active_node)["GroupPublisher"])
+                    core.set_attribute(new_group_pub, 'name', name)
+                    core.set_attribute(new_group_pub, 'nodeName', parent_name)
+                if meta_type == "Subscriber":
+                    new_group_sub = core.create_child(g, self.util.META(active_node)["GroupSubscriber"])
+                    core.set_attribute(new_group_sub, 'name', name)
+                    core.set_attribute(new_group_sub, 'nodeName', parent_name)
+                
+            self.util.traverse(g, create_group_pub_sub)
+        
         
         # Set up empty dictionary for pubs and subs
         pub_dict = dict()
@@ -84,11 +126,11 @@ class MakeConnections(PluginBase):
         # Set up publisher and subscriber dictionary before remap
         for p in publishers:
             name = core.get_attribute(p, 'name')
-            pub_dict[p["nodePath"]] = {"old_name": name, "remap_name": name}
+            pub_dict[p["nodePath"]] = {"node": p, "old_name": name, "remap_name": name}
             
         for s in subscribers:
             name = core.get_attribute(s, 'name')
-            sub_dict[s["nodePath"]] = {"old_name": name, "remap_name": name}
+            sub_dict[s["nodePath"]] = {"node": s,"old_name": name, "remap_name": name}
             
         def count_slashes(string):
             return string["nodePath"].count('/')
@@ -100,7 +142,7 @@ class MakeConnections(PluginBase):
 
             r_from = core.get_attribute(r, 'from')
             r_to = core.get_attribute(r, 'to')
-        
+            
             def remap_fcn(node):
                 meta_node = core.get_base_type(node)
                 meta_type = core.get_attribute(meta_node, 'name')
@@ -111,13 +153,13 @@ class MakeConnections(PluginBase):
                     if node["nodePath"] in sub_dict:
                         if sub_dict[node["nodePath"]]["remap_name"] == r_from:
                             sub_dict[node["nodePath"]]["remap_name"] = r_to
-            
+                
             children = core.load_children(r_parent)
             for c in children:
                 self.util.traverse(c, remap_fcn)
-        
-        logger.info(pub_dict)
-        logger.info(sub_dict)
+            
+        logger.info(f"pub dict: {pub_dict}")
+        logger.info(f"sub dict: {sub_dict}")
         
         # Draw new topic connections
         def connect_at_node(node):
@@ -125,17 +167,46 @@ class MakeConnections(PluginBase):
             meta_type = core.get_attribute(meta_node, 'name')
             if not(meta_type == "LaunchFile" or meta_type == "Group"):
                 return
-        
+            
             pubs, subs = get_connectable_ports(node)
-        
+            
             for p in pubs:
-                for s in subs:
-                    p_topic = pub_dict[p["nodePath"]]["remap_name"]
-                    s_topic = sub_dict[s["nodePath"]]["remap_name"]
+                p_topic = None
+                p_meta = core.get_base_type(p)
+                p_type = core.get_attribute(p_meta, 'name')
                 
-                    if p_topic == s_topic:
+                if p_type == "Publisher":
+                    p_topic = pub_dict[p["nodePath"]]["remap_name"]
+                if p_type == "GroupPublisher":
+                    for nodePath, names in pub_dict.items():
+                        orig_pub = names["node"]
+                        parent = core.get_parent(orig_pub)
+                        if names["old_name"] == core.get_attribute(p, "name") and core.get_attribute(parent, "name") == core.get_attribute(p, "nodeName"):
+                            p_topic = names["remap_name"]
+                
+                for s in subs:
+                    s_topic = None
+                    s_meta = core.get_base_type(s)
+                    s_type = core.get_attribute(s_meta, 'name')
+                    
+                    if s_type == "Subscriber":
+                        s_topic = sub_dict[s["nodePath"]]["remap_name"]
+                    if s_type == "GroupSubscriber":
+                        for nodePath, names in sub_dict.items():
+                            orig_sub = names["node"]
+                            parent = core.get_parent(orig_sub)
+                            if names["old_name"] == core.get_attribute(s, "name") and core.get_attribute(parent, "name") == core.get_attribute(s, "nodeName"):
+                                s_topic = names["remap_name"]
+                                
+                    p_path_length = count_slashes(p)
+                    s_path_length = count_slashes(s)
+                
+                    common_parent = core.get_common_parent([p, s])
+                    parent_length = count_slashes(common_parent)
+                
+                    if p_topic == s_topic and p_path_length == s_path_length and parent_length + 2 == p_path_length:
                         draw_connection(p, s, p_topic)
-        
+                
         self.util.traverse(active_node, connect_at_node)
         
         # Save updates
@@ -147,3 +218,4 @@ class MakeConnections(PluginBase):
         )
         
         logger.info("DONE")
+        
