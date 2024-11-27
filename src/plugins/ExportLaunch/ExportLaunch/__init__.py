a505b9582cc3e3f2a256b970f1dfe472e7d77296
tanujdebug <your.email@example.com>
2024-11-27 12:47:37 -0600

    export launch

--- src/plugins/ExportLaunch/ExportLaunch/__init__.py
+++ src/plugins/ExportLaunch/ExportLaunch/__init__.py
@@ -16,17 +16,79 @@ handler.setFormatter(formatter)
 logger.addHandler(handler)
 
 
-class ExportLaunch(PluginBase):
+class ExportLaunch(PluginBase,indent=0):
     def main(self):
-        core = self.core
-        root_node = self.root_node
         active_node = self.active_node
+        core = self.core
+        logger = self.logger
+        
+        visited_nodes = []
+        ignoreMetaType = ["GroupPublisher","GroupSubscriber","Subscriber","Topic","Publisher"]
+        nodeLibrary = []
+        def xmlGenerator(activeNode,indent = 0,topLevel = True):
+            
+            result = ""
+            node_path = core.get_path(activeNode)
+            
+            if topLevel:
+                result += " " * indent + "\n<launch>\n"
+                
+            if node_path in visited_nodes:
+                #logger.warning(f"Node already visited, skipping: {node_path}")
+                return result +'\n</launch>'
+            
+            visited_nodes.append(node_path)
+            children = core.load_children(activeNode)
+            
+            for child in children:
+                
+                meta_type = core.get_meta_type(child)
+                metaTypeName = core.get_attribute(meta_type,'name')
+                childName = core.get_attribute(child,'name')
+                logger.info(f"metaTypeName {metaTypeName}")
+                
+                if metaTypeName in ignoreMetaType:
+                    continue
+                #logger.info(f"{core.get_attribute(child, 'pkg')} {core.get_attribute(child, 'type')} {core.get_attribute(child, 'args')} {core.get_attribute(child, 'name')} core.get_attribute(child, 'args')")
+                if metaTypeName == "Argument":
+                    arg_name = core.get_attribute(child, 'name')
+                    arg_value = core.get_attribute(child, 'value')
+                    logger.info(f"{arg_name} {arg_value}")
+                    result += f"{' ' * (indent + 2)}<arg name=\"{arg_name}\" value=\"{arg_value}\"/>\n"
+                    #result += f"{' ' * indent}{childName}()\n"
+                
+                elif core.get_attribute(child, 'pkg') or core.get_attribute(child, 'type') or core.get_attribute(child, 'args') or core.get_attribute(child, 'respawn'):
+                    #pkg = "operations"
+                    result += f"{' ' * (indent + 2)}<node name=\"{childName}\" pkg=\"{core.get_attribute(child, 'pkg')}\" type=\"{metaTypeName}\" args=\"{ core.get_attribute(child, 'args')}\">\n"
+                    result += xmlGenerator(child, indent + 4,topLevel = False)
+                    result += f"{' ' * (indent + 2)}</node>\n"
 
-        name = core.get_attribute(active_node, 'name')
+                elif metaTypeName == "Remap":
+                    remap_from = core.get_attribute(child, 'from')
+                    remap_to = core.get_attribute(child, 'to')
+                    result += f"{' ' * (indent + 2)}<remap from=\"{remap_from}\" to=\"{remap_to}\"/>\n"    
+                
+                elif metaTypeName == "Include":
+                    file_name = core.get_attribute(child, 'name')
+                    if '.launch' not in file_name:
+                        logger.info(f"{file_name}")
+                        result += f"{' ' * (indent + 2)}<include file=\"{file_name}.launch\"/>\n"
+                    else:
+                        result += f"{' ' * (indent + 2)}<include file=\"{file_name}\"/>\n"
+                
+                elif metaTypeName == "Group":
+                    ns = core.get_attribute(child, 'name')
+                    result += f"{' ' * (indent + 2)}<group ns=\"{ns}\">\n"
+                    # Recursively process children of the group
+                    result += xmlGenerator(child, indent + 4,topLevel = False)
+                    result += f"{' ' * (indent + 2)}</group>\n"
+                
+            if topLevel:
+                result += " " * indent + "</launch>\n"
+            return result
+        output = xmlGenerator(active_node)
+        logger.info(f"output {output}")
+        file_name = 'output.xml'
+        file_hash = self.add_file(file_name, output)
+        logger.info(f"Output saved to file with hash: {file_hash}")
 
-        logger.info('ActiveNode at "{0}" has name {1}'.format(core.get_path(active_node), name))
-
-        core.set_attribute(active_node, 'name', 'newName')
-
-        commit_info = self.util.save(root_node, self.commit_hash, 'master', 'Python plugin updated the model')
-        logger.info('committed :{0}'.format(commit_info))
